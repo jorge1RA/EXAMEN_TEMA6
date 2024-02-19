c5f7736dbc713df13ff0a83d6a83104b4bfdbe64
jorge <jromari069@g.educaand.es>
2024-02-19 11:06:47 +0100

    actualizacion

--- index.js
+++ index.js
@@ -1,13 +1,153 @@
-const http = require("http");
+// Importamos las bibliotecas necesarias. 
+// Concretamente el framework express
+const express = require('express');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.end("<h1>EXAMEN_TEMA6_JRA</h1>");
+// Inicializamos la aplicación
+const app = express();
+
+// Indicamos que la aplicación puede recibir JSON (API Rest)
+app.use(express.json());
+
+// Indicamos el puerto en el que vamos a desplegar la aplicación
+const port = process.env.PORT || 8080;
+
+// Arrancamos la aplicación
+app.listen(port, () => {
+    console.log('Servidor desplegado por ' + port);
 });
 
-const port = 8080; // Usando 8080 para coincidir con la configuración de Docker
+// Definimos una estructura de datos 
+// (temporal hasta incorporar una base de datos)
+let concesionarios = [
+    {nombre: "Opel", localizacion: "C/ Falsa 123", 
+        coches: [{modelo: "Corsa", cv:75, precio: 12345 }] },
+    {nombre: "Ford", localizacion: "C/ Falsa 456", 
+        coches: [{modelo: "Fiesta", cv:90, precio: 13425 }] },
+    {nombre: "Peugeot", localizacion: "C/ Falsa 789", 
+        coches: [{modelo: "208", cv:110, precio: 15432 }] },
+]
 
-server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+app.get('/concesionarios',  (req, res) => {
+    res.json(concesionarios);
+});
+
+app.post('/concesionarios', (req, res) => {
+    concesionarios.push(req.body);
+    res.json(concesionarios);
+});
+
+app.get('/concesionarios/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    console.log(concesionarios.length)
+    const isIdInvalido = isNaN(id) || id >= concesionarios.length || id < 0;
+
+    if(isIdInvalido) {
+        res.status(404).json({ error: 404, message: "Id inválida" });
+    } else {
+        const result = concesionarios[id];
+        res.json(result);
+    }
+});
+
+app.put('/concesionarios/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const isIdInvalido = isNaN(id) || id >= concesionarios.length || id < 0;
+    
+    if(isIdInvalido) {
+        res.status(404).json({ error: 404, message: "Id inválida" });
+    } else {
+        concesionarios[id] = req.body;
+        res.json(concesionarios);
+    }
+});
+
+app.delete('/concesionarios/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const isIdInvalido = isNaN(id) || id >= concesionarios.length || id < 0;
+    
+    if(isIdInvalido) {
+        res.status(404).json({ error: 404, message: "Id inválida" });
+    } else {
+        concesionarios = concesionarios.filter(item => concesionarios.indexOf(item) !== id);
+
+        res.json(concesionarios);
+    }
+});
+
+
+
+// --------------------------------------
+
+app.get('/concesionarios/:id/coches', (req, res) => {
+    const id = parseInt(req.params.id);
+    const isIdInvalido = isNaN(id) || id >= concesionarios.length || id < 0;
+    
+    if(isIdInvalido) {
+        res.status(404).json({ error: 404, message: "Id inválida" });
+    } else {
+        const result = concesionarios[id].coches;
+        
+        res.json(result);
+    }
+});
+
+
+app.post('/concesionarios/:id/coches', (req, res) => {
+    const id = parseInt(req.params.id);
+    const isIdInvalido = isNaN(id) || id >= concesionarios.length || id < 0;
+    
+    if(isIdInvalido) {
+        res.status(404).json({ error: 404, message: "Id inválida" });
+    } else {
+        concesionarios[id].coches.push(req.body);
+        const result = concesionarios[id].coches;
+        res.json(result);
+    }
+});
+
+
+
+app.get('/concesionarios/:id/coches/:cocheId', (req, res) => {
+    const id = parseInt(req.params.id);
+    const cocheId = parseInt(req.params.cocheId);
+    const isIdInvalido = isNaN(id) || id >= concesionarios.length || id < 0;
+    
+    if(isIdInvalido) {
+        res.status(404).json({ error: 404, message: "Id inválida" });
+    } else {
+        const result = concesionarios[id].coches[cocheId];
+        res.json(result);
+    }
+});
+
+app.put('/concesionarios/:id/coches/:cocheId', (req, res) => {
+    const id = parseInt(req.params.id);
+    const cocheId = parseInt(req.params.cocheId);
+    const isIdInvalido = isNaN(id) || id >= concesionarios.length || id < 0;
+    
+    if(isIdInvalido) {
+        res.status(404).json({ error: 404, message: "Id inválida" });
+    } else {
+        concesionarios[id].coches[cocheId] = req.body;
+        const result = concesionarios[id].coches;
+        res.json(result);
+    }
+});
+
+
+
+app.delete('/concesionarios/:id/coches/:cocheId', (req, res) => {
+    const id = parseInt(req.params.id);
+    const cocheId = parseInt(req.params.cocheId);
+    const isIdInvalido = isNaN(id) || id >= concesionarios.length || id < 0;
+    
+    if(isIdInvalido) {
+        res.status(404).json({ error: 404, message: "Id inválida" });
+    } else {
+        let coches = concesionarios[id].coches;
+        coches = coches.filter(item => coches.indexOf(item) !== cocheId);
+        concesionarios[id].coches = coches;
+
+        res.json(concesionarios[id].coches);
+    }
 });
